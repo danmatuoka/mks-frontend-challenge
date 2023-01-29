0018fa028c0bdaaed5d92797cef50b98c9fdd71a
danmatuoka <dan_mtk@hotmail.com>
2023-01-29 15:33:01 -0300

    fix: global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Montserrat } from "@next/font/google";
 import { Provider } from "react-redux";
 import { store } from "../store";
+import Global from "../styles/global";
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <main className={montserrat.className}>
+        <Global />
         <Component {...pageProps} />
       </main>
     </Provider>
---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html,
+body {
+  max-width: 100vw;
+  overflow-x: hidden;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+button {
+    cursor: pointer;
+}
+
+`;
